b9282eb451f65496973cf6277e6a45cd296e0079
KKM1502 <102860627+KKM1502@users.noreply.github.com>
2022-07-29 19:41:22 +0900

    object
    object

--- html/this.js
+++ html/this.js
@@ -1,19 +1,24 @@
+
+function setColor(color){
+  var everya = document.querySelectorAll("a");
+      for (let i = 0; i < everya.length; i++) {
+        everya[i].style.color = color;
+        
+      }
+      document.querySelector("body").style.color = color;
+  }
+  function SetBackgroundColor(color){
+    document.querySelector("body").style.backgroundColor = color;
+  }
 function nightDayHandler(self) {
     var target = document.querySelector("body");
     if (self.value === "night") {
       target.style.backgroundColor = "black";
-      target.style.color = "white";
       self.value = "day";
-      var everya = document.querySelectorAll("a");
-      for (let i = 0; i < everya.length; i++) {
-        everya[i].style.color = "white";
-      }
+      setColor("white")
     } else {
       target.style.backgroundColor = "white";
-      target.style.color = "black";
       self.value = "night";
-      for (let i = 0; i < everya.length; i++) {
-        everya[i].style.color = "black";
-      }
+      setColor("black")
     }
   }
