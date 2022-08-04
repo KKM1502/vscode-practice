9e98bf5cf4c0331c90bb9daa549a018cacbd68b8
KKM1502 <102860627+KKM1502@users.noreply.github.com>
2022-08-05 00:52:20 +0900

    함수명 변경

--- html_practice/아트보드 – 1/bghandler.js
+++ html_practice/아트보드 – 1/bghandler.js
@@ -1,5 +1,5 @@
 
-function nightDayHandler(self) {
+function Bghandler(self) {
     if (self.value === "Train") {
     document.getElementById("intro_bg").style.background="url('car.gif')"
       self.value = "Car";
